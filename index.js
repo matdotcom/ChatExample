c4294ad2cddcd5ad9157413bdb7a8257dfec6452
mathiasbruun <math686f@edu.easj.dk>
2018-09-24 14:54:15 +0200

    hajajajaja

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-io.on('connection', function(socket){
     socket.on('chat message', function(msg){
       console.log('message: ' + msg);
     });
