790d57b901158a80f11276419bd9b04f85f83fa8
mathiasbruun <math686f@edu.easj.dk>
2018-09-29 14:36:52 +0200

    ye chat med username lmao

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-var http = require('http');
-var fs = require('fs');
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'), // Blocks HTML characters (security equivalent to htmlentities in PHP)
+    fs = require('fs');
 
-// Loading the file index.html displayed to the client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
+// Loading the page index.html
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-// Loading socket.io
-var io = require('socket.io').listen(server);
-
 io.sockets.on('connection', function (socket, username) {
-    // When the client connects, they are sent a message
-    socket.emit('message', 'You are connected!');
-    // The other clients are told that someone new has arrived
-    socket.broadcast.emit('message', 'Another client has just connected!');
-
-    // As soon as the username is received, it's stored as a session variable
-    socket.on('little_newbie', function(username) {
+    // When the username is received it’s stored as a session variable and informs the other people
+    socket.on('new_client', function(username) {
+        username = ent.encode(username);
         socket.username = username;
+        socket.broadcast.emit('new_client', username);
     });
 
-    // When a "message" is received (click on the button), it's logged in the console
+    // When a message is received, the client’s username is retrieved and sent to the other people
     socket.on('message', function (message) {
-        // The username of the person who clicked is retrieved from the session variables
-        console.log(socket.username + ' is speaking to me! They\'re saying: ' + message);
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {username: socket.username, message: message});
     }); 
 });
 
-
-server.listen(8080);
+server.listen(8080);  
